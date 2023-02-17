dcae16b3420532029d1d0cad1c5fd1033e7ab3f8
Mathias Javier Murillo <125533815+mathijm@users.noreply.github.com>
2023-02-16 21:59:39 -0600

    Initial-Released

---
+++ script.js
@@ -0,0 +1,8 @@
+setTimeout(cambiaMensaje,5300)
+function cambiaMensaje(){
+document.getElementById("loading-message").innerText="Enviado a mi amorcito!";
+}
+
+setTimeout(function (){
+    alert("Recibiste un abrazo!")
+},5400)
